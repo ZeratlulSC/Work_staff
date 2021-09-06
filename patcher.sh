ccf4d825e101d7d60643ce053b24e64b0545510f
ZeratlulSC <nikolai.klokov@gmail.com>
2021-09-06 14:19:27 +0300

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-patchelf --set-interpreter /home/user/lib/ld-linux-armhf.so.3 --set-rpath /home/user/lib/arm-linux-gnueabihf/libc.so.6 /work/gsrv/g_taskman
+# list everything in pwd (home by now)
 
 for n in /home/*; do printf '%s\n' "$n";
 done
